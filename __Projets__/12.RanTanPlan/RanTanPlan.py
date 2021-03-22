f87565ef1a7a939b3ae7b9d244b4e6e170b8be70
ThomasLENNE <thomas.lenne@gmail.com>
2021-03-22 21:13:33 +0100

    projets

---
+++ __Projets__/12.RanTanPlan/RanTanPlan.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#Rantanplan
+
+#Pendant qu’Averell finit de manger, les trois autres Dalton prennent l’air dans un champ .
+#Rantanplan traine aussi dans le champ.
+#Les trois Dalton appellent simultanément le chien qui se dirige aléatoirement vers l’un d’eux.
+#Trahi par sa mémoire défaillante, il s’arrête au milieu de son trajet et creuse un trou.
+#Les trois Dalton le rappellent, il repart au hasard vers l’un d’eux , s’arrête à mi-chemin , creuse un trou…..
+#Afficher l’ensemble des trous faits par Rantanplan lorsqu’il est appelé n fois. On pourra:
+# -Réaliser une fonction qui prend en argument les coordonnées de rantanplan et qui renvoie les coordonnées du trou suivant
+# -Réaliser une fonction qui affiche les n trous.
+#Afficher 10000 trous. Epatant non ?
+
+from random import *
+from matplotlib import pyplot
+from math import *
+
